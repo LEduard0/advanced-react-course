2f647123942ae0558e41878306d2dedf74b32206
¨Du¨ <leduardo152@gmail.com>
2024-10-01 21:48:12 -0300

    feat: add eslint configs

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  { rules: {
+    "react/react-in-jsx-scope": false,
+    "react-hooks/rules-of-hooks": "error",
+    "react/props-types": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off"
+  }},
+  {extends: "next/core-web-vitals"}
+];
