b4fe9beb7d1275821ad7a3a3deeae19f469f79c7
¨Du¨ <leduardo152@gmail.com>
2024-10-02 17:04:35 -0300

    teste lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,26 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser}, parserOptions: {
-    warnOnUnsupportedTypeScriptVersion: false
-  }},
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  {
+    languageOptions: { globals: globals.browser },
+    parserOptions: {
+      warnOnUnsupportedTypeScriptVersion: false
+    }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  { rules: {
-    "react/react-in-jsx-scope": false,
-    "react-hooks/rules-of-hooks": "error",
-    "react/props-types": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off"
-  }},
-  {extends: ["next/core-web-vitals", "prettier"]}
-];
+  {
+    rules: {
+      'react/react-in-jsx-scope': false,
+      'react-hooks/rules-of-hooks': 'error',
+      'react/props-types': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off'
+    }
+  },
+  { extends: ['next/core-web-vitals', 'prettier'] }
+]
